3367665895a7591709cdce20fd722bff5e11b1b6
Aadya Bhattarai <aadyab@usf.edu>
2025-12-04 03:17:52 -0500

    Initialize
    Added module documentation and versioning information.

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,27 @@
+"""
+Vehicle Platooning Simulation Package
 
+A modular framework for simulating vehicle platooning and car-following
+scenarios using SUMO and Plexe.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+
+from .core import SimulationBase, NetworkGenerator, RouteGenerator
+from .platooning import PlatooningSim, PlatoonController
+from .car_following import CarFollowingSim
+from .utils import ConfigLoader, ExcelWriter, CRRModifier, Statistics
+
+__all__ = [
+    "SimulationBase",
+    "NetworkGenerator",
+    "RouteGenerator",
+    "PlatooningSim",
+    "PlatoonController",
+    "CarFollowingSim",
+    "ConfigLoader",
+    "ExcelWriter",
+    "CRRModifier",
+    "Statistics",
+]
