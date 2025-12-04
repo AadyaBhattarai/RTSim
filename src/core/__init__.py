12ed1ee381cef5db9989e75800ac069955e56e4d
Aadya Bhattarai <aadyab@usf.edu>
2025-12-04 03:16:34 -0500

    adding __init__.py
    Added module docstring and updated __all__ exports.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,1 +1,9 @@
+"""
+Core simulation modules for the vehicle platooning simulation framework.
+"""
 
+from .simulation_base import SimulationBase
+from .network_generator import NetworkGenerator
+from .route_generator import RouteGenerator
+
+__all__ = ["SimulationBase", "NetworkGenerator", "RouteGenerator"]
