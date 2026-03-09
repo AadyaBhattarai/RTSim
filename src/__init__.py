231509b08f2f80a477eadf529bae23b78c4b0fa8
Aadya Bhattarai <aadyab@usf.edu>
2026-03-09 01:32:43 -0400

    Refactor __all__ exports in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-"""Vehicle Platooning and Car Following Simulation Package."""
+"""RTSim: Road-Trip Simulator for fuel consumption of ICEVs with automation."""
 
 from .core import SimulationBase, NetworkGenerator, RouteGenerator
 from .platooning import PlatooningSim, run_platooning_simulation
@@ -6,19 +6,9 @@ from .car_following import CarFollowingSim, run_car_following_simulation
 from .utils import CRRModifier, calculate_confidence_interval, append_df_to_excel, sanitize_workbook
 
 __all__ = [
-    # Core (shared)
-    "SimulationBase",
-    "NetworkGenerator",
-    "RouteGenerator",
-    # Platooning
-    "PlatooningSim",
-    "run_platooning_simulation",
-    # Car Following
-    "CarFollowingSim",
-    "run_car_following_simulation",
-    # Utils
-    "CRRModifier",
-    "calculate_confidence_interval",
-    "append_df_to_excel",
-    "sanitize_workbook",
+    "SimulationBase", "NetworkGenerator", "RouteGenerator",
+    "PlatooningSim", "run_platooning_simulation",
+    "CarFollowingSim", "run_car_following_simulation",
+    "CRRModifier", "calculate_confidence_interval",
+    "append_df_to_excel", "sanitize_workbook",
 ]
